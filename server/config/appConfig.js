f52fdc841e55d0c1d5097ea2d7cb3f29a1cf64ae
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-09-09 21:42:26 +0530

    env variables added

--- server/config/appConfig.js
+++ server/config/appConfig.js
@@ -4,4 +4,7 @@ module.exports={
     DB_CONNECTION_STRING : process.env.DB_CONNECTION_STRING,
     PORT : process.env.PORT,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    FORGOT_PASSWORD_EXPIRY: process.env.FORGOT_PASSWORD_EXPIRY,
+    TOKEN_EXPIRY: process.env.TOKEN_EXPIRY
 }
