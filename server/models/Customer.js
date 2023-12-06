547aab2acd2d56ceb01c1d1143c10fb8833aa056
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-06 16:11:32 +0530

    updated customer model

--- server/models/Customer.js
+++ server/models/Customer.js
@@ -4,15 +4,29 @@ const customerSchema = new mongoose.Schema({
     user_id:{
         type:mongoose.SchemaTypes.ObjectId,
         ref:"User",
+        required:[true, "User Not Registered"]
     },
-    currentOrders:{
-
-    },
+    currentOrders:[
+        {
+            orderId : mongoose.SchemaTypes.ObjectId,
+            ref: "Order"
+        }
+    ],
+    pastOrders: [
+        {
+            orderId : mongoose.SchemaTypes.ObjectId,
+            ref: "Order"
+        }
+    ],
     favRestaurants: {
         type:Array,
         default:[]
+    },
+    location: {
+        latitude: String,
+        longitude: String
     }
-});
+},{versionKey: false});
 
 const Customer = mongoose.model("Customer" , customerSchema);
 module.exports = Customer;
