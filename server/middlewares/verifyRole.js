732c1c552e1929935eb28fc7d4cd93945d435d7e
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-13 17:58:38 +0530

    added dynamic roles check

--- server/middlewares/verifyRole.js
+++ server/middlewares/verifyRole.js
@@ -1,7 +1,7 @@
 module.exports = verifyRole = (roles) => {
     return (req, res, next) => {
         const userRole = req.user.role;
-        const isAuthorized = Array.isArray(roles) ? roles.includes(userRole) : userRole != roles;
+        const isAuthorized = Array.isArray(roles) ? roles.includes(userRole) : userRole === roles;
         if(!isAuthorized) {
             return res.status(403).send("Access Denied for this resource");
         }
