074f8e9e4141225e7f23d3019fec9e5012e5080b
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-17 23:45:33 +0530

    fixed synchronomous cookie and header issue

--- server/helpers/utils/cookieToken.js
+++ server/helpers/utils/cookieToken.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
 const { TOKEN_EXPIRY, JWT_SECRET_KEY } = require("../../config/appConfig");
-module.exports = cookieToken = async (user, res, message) => {
-  console.log("Cookie User : ", user);
+
+module.exports = async function cookieToken(user, res, message) {
+  console.log("Cookie User: ", user);
+
   const token = jwt.sign({ id: user._id }, JWT_SECRET_KEY, {
     expiresIn: TOKEN_EXPIRY,
   });
-  res
-    .status(200)
-    .cookie("access_token", token, { 
-      expiresIn: new Date(Date.now() + TOKEN_EXPIRY),
-    })
-    .json({ success: true, token, user, message });
-}
+
+  await res.cookie("access_token", token, {
+    expiresIn: new Date(Date.now() + TOKEN_EXPIRY),
+    httpOnly: true,
+  });
+
+  await res.status(200).json({ success: true, token, user, message });
+};
