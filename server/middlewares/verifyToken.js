b044e96f81cb68bf1ef3dce7ec7a247489bfbee1
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-18 13:30:53 +0530

    tabbings

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-function verifyToken(req,res,next){
+function verifyToken(req, res, next) {
     //first get the token from the request header's cookies and check if available
     const token = req.cookies.access_token;
-    if(!token){
-       return res.status(401).send("You are not Authenticated");
+    if (!token) {
+        return res.status(401).send("You are not Authenticated");
     }
     //now verify token with the jwt private key
-    jwt.verify(token , process.env.JWT_SECRET_KET , (err, user)=>{
-        if(err){
+    jwt.verify(token, process.env.JWT_SECRET_KET, (err, user) => {
+        if (err) {
             return res.status(403).send('Invalid Token');
         }
         req.user = user;
