a06e2e22a46da4d59d212e5f202eb5321fc305df
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2025-03-04 10:37:46 +0530

    fixed drawer issues and updated design

--- client/src/Components/Menubars/Drawer.jsx
+++ client/src/Components/Menubars/Drawer.jsx
@@ -1,32 +1,30 @@
 import { SheetTrigger, SheetContent, Sheet } from "../ui/sheet.jsx";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { Button } from "../ui/button.jsx";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { logout } from "../../Redux/Slices/authSlice.js";
 import { useDispatch, useSelector } from "react-redux";
 import { AiFillHome } from "react-icons/ai";
 import { HiShoppingBag } from "react-icons/hi2";
+import { MdDeliveryDining } from "react-icons/md";
+import { FaFileInvoice } from "react-icons/fa6";
 import { FaRegUser } from "react-icons/fa";
 import MenuBarTab from "./MenuBarTab.jsx";
 
 const CustomerTabs = [
     { itemname: "Home", link: "/", icon: <AiFillHome /> },
     { itemname: "Profile", link: "/profile", icon: <FaRegUser /> },
-    { itemname: "My Orders", link: "/myorders", icon: <HiShoppingBag /> },
+    { itemname: "My Orders", link: "/myorder", icon: <HiShoppingBag /> },
 ];
 
 export default function Drawer() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { isLoggedIn, role, data } = useSelector((state) => state.auth);
-    const { restaurantData } = useSelector((state) => state.restaurant);
+    const { restaurant } = useSelector((state) => state.restaurant);
 
     async function handleLogout() {
         await dispatch(logout());
-    }
-
-    function RestaurantExist(userId) {
-        return restaurantData?.some((res) => res?.user_id === userId);
     }
 
     return (
@@ -43,10 +41,7 @@ export default function Drawer() {
             </SheetTrigger>
             <SheetContent side="left">
                 <div className="p-2">
-                    <div
-                        to="/"
-                        className="flex items-start justify-start gap-2"
-                    >
+                    <div className="flex items-start justify-start gap-2">
                         <img
                             src="https://res.cloudinary.com/ddxq9mouk/image/upload/v1715963080/Portfolio/Food%20Ordering%20App/z47evx2lisarubrteuxm.jpg"
                             width="34"
@@ -55,51 +50,74 @@ export default function Drawer() {
                         />
                         <span className="text-lg font-semibold">Chew Chew</span>
                     </div>
-                    <div className="">
-                        {isLoggedIn && role === "Customer" && (
-                            <div className="flex flex-col h-[120px] justify-between mt-6">
-                                <div className="flex flex-col gap-4 my-4">
-                                    {CustomerTabs.map((tab, index) => {
-                                        return (
-                                            <MenuBarTab key={index} {...tab} />
-                                        );
-                                    })}
-                                </div>
-                                <Button
-                                    variant="outline "
-                                    onClick={handleLogout}
-                                    className="hover:text-custom-red-2 text-custom-red-1 mt-6 font-semibold border"
-                                >
-                                    Sign out
-                                </Button>
+
+                    {isLoggedIn && role === "Customer" && (
+                        <div className="flex flex-col h-[120px] justify-between mt-6">
+                            <div className="flex flex-col gap-4 my-4">
+                                {CustomerTabs.map((tab, index) => (
+                                    <MenuBarTab key={index} {...tab} />
+                                ))}
                             </div>
-                        )}
-                    </div>
-                    {/* {isLoggedIn && (
+                            <Button
+                                variant="outline"
+                                onClick={handleLogout}
+                                className="hover:text-custom-red-2 text-custom-red-1 mt-6 font-semibold border"
+                            >
+                                Sign out
+                            </Button>
+                        </div>
+                    )}
+
+                    {isLoggedIn && role === "Restaurant" && (
                         <>
                             <Link to="/profile">Profile</Link>
-                            {role === "Restaurant" &&
-                                !RestaurantExist(data?._id) && (
-                                    <Link to="/create/Restaurant">
-                                        Add Restaurant
-                                    </Link>
-                                )}
                             <Link to="/myorder">My Order</Link>
-                            <button onClick={handleLogout}>Logout</button>
+                            {restaurant && (
+                                <>
+                                    <MenuBarTab
+                                        itemname="My Restaurant"
+                                        link={`/restaurant/details/${restaurant._id}`}
+                                        icon={<FaRegUser />}
+                                    />
+                                    <MenuBarTab
+                                        itemname="Orders"
+                                        link="/myorder"
+                                        icon={<HiShoppingBag />}
+                                    />
+                                    <MenuBarTab
+                                        itemname="DeliveryMen"
+                                        link="/restaurant/deliverymen"
+                                        icon={<MdDeliveryDining />}
+                                    />
+                                    <MenuBarTab
+                                        itemname="Reports"
+                                        link="/reports"
+                                        icon={<FaFileInvoice />}
+                                    />
+                                </>
+                            )}
+                            <Button
+                                variant="outline"
+                                onClick={handleLogout}
+                                className="hover:text-custom-red-2 text-custom-red-1 mt-6 font-semibold border"
+                            >
+                                Sign out
+                            </Button>
                         </>
-                    )} */}
+                    )}
+
                     {!isLoggedIn && (
-                        <div className="flex flex-col md:gap-4 mt-14  gap-2 sm:gap-2">
+                        <div className="flex flex-col gap-4 mt-14">
                             <Button
                                 variant="outline"
                                 onClick={() => navigate("/login")}
-                                className=" hover:text-custom-red-2 text-custom-red-1 rounded-xl font-semibold"
+                                className="hover:text-custom-red-2 text-custom-red-1 font-semibold"
                             >
                                 Login
                             </Button>
                             <Button
                                 onClick={() => navigate("/signup")}
-                                className="bg-custom-red-1 hover:bg-custom-red-2 text-white rounded-xl font-semibold"
+                                className="bg-custom-red-1 hover:bg-custom-red-2 text-white font-semibold"
                             >
                                 Sign Up
                             </Button>
