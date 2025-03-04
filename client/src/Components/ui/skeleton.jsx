747d966e219a015eb03bc0eb36d73a2c74da4be6
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2025-03-04 10:38:44 +0530

    added new shadcn components

---
+++ client/src/Components/ui/skeleton.jsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils"
+
+function Skeleton({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      className={cn("animate-pulse rounded-md bg-zinc-900/10 dark:bg-zinc-50/10", className)}
+      {...props} />)
+  );
+}
+
+export { Skeleton }
